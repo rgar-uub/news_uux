f77d99db4da5e6de05ed368acf6c4a980dc5c4bc
rgar-uub <54169859+rgar-uub@users.noreply.github.com>
2019-09-20 14:36:31 +0300

    Update scraper.js
    again sql

--- scraper.js
+++ scraper.js
@@ -2,6 +2,7 @@ var needle = require('needle');
 var cheerio = require('cheerio');
 var tress = require('tress');
 var resolve = require('url').resolve;
+var sqlite3 = require('sqlite3').verbose();
 
 var startURL = 'https://www.buzzbuzzhome.com/city/united-states/maryland/baltimore';
 var results = [];
@@ -33,7 +34,23 @@ function work(url, cb){
 }
 
 function done(){
-    // тут что-то делаем с массивом результатов results
-    // например, выводит в консоль на этапе тестирования
-    console.log(results);
+    var db = new sqlite3.Database('data.sqlite');
+    db.serialize(function(){
+        db.run('DROP TABLE IF EXISTS new');
+        db.run('CREATE TABLE new (url TEXT, name TEXT, price TEXT)');
+        var stmt = db.prepare('INSERT INTO new VALUES (?, ?, ?)');
+        for (var i = 0; i < results.length; i++) {
+            stmt.run(results[i]);
+        };
+        stmt.finalize();
+        db.run('CREATE TABLE IF NOT EXISTS data (url TEXT, name TEXT, price TEXT, state TEXT)');
+        db.run('UPDATE data set state = NULL');
+        db.run('INSERT INTO data SELECT url, name, price, "new" AS state FROM new ' +
+            'WHERE url IN (SELECT url FROM new EXCEPT SELECT url FROM data)');
+        db.run('DELETE FROM data WHERE url IN (SELECT url FROM data EXCEPT SELECT url FROM new)');
+        db.run('UPDATE data SET state = "upd", price = (SELECT price FROM new WHERE new.url = data.url) ' +
+            'WHERE url IN (SELECT old.url FROM data AS old, new WHERE old.url = new.url AND old.price <> new.price)');
+        db.run('DROP TABLE new');
+        db.close();
+    });
 }
